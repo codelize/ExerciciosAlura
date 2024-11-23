063e94583db77233fee96e5a5f088fa6bafce857
codelize <lflizier@gmail.com>
2024-11-23 17:28:18 -0300

    Exercicio 3

---
+++ src/Exercicio3.java
@@ -0,0 +1,8 @@
+public class Exercicio3 {
+    public static void main(String[] args) {
+        char letra = 'A';
+        String palavra = "Java";
+
+        System.out.println("Mensagem concatenada: " + letra + " - " + palavra);
+    }
+}
