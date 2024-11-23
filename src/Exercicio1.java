60cf1fb2909ca85afb79fb41acab39b6891e4834
codelize <lflizier@gmail.com>
2024-11-23 17:27:38 -0300

    Exercicio 1

---
+++ src/Exercicio1.java
@@ -0,0 +1,9 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        double nota1 = 8.5;
+        double nota2 = 7.3;
+        double media = (nota1 + nota2) / 2;
+
+        System.out.println("A média das notas é: " + media);
+    }
+}
