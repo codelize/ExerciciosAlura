2768af54b2d0a80eed13f86df4dc0d4b179f82af
codelize <lflizier@gmail.com>
2024-11-23 17:27:59 -0300

    Exercicio 2

---
+++ src/Exercicio2.java
@@ -0,0 +1,9 @@
+public class Exercicio2 {
+    public static void main(String[] args) {
+        double numeroDouble = 7.9;
+        int numeroInt = (int) numeroDouble; // Casting de double para int
+
+        System.out.println("Número original (double): " + numeroDouble);
+        System.out.println("Número convertido para int: " + numeroInt);
+    }
+}
