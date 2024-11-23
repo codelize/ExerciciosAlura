05ff0d469b77f0ce8d6068812122ceff80ab0b25
codelize <lflizier@gmail.com>
2024-11-23 17:28:30 -0300

    Exercicio 4

---
+++ src/Exercicio4.java
@@ -0,0 +1,10 @@
+public class Exercicio4 {
+    public static void main(String[] args) {
+        double precoProduto = 19.99;
+        int quantidade = 5;
+
+        double valorTotal = precoProduto * quantidade;
+
+        System.out.println("O valor total é: R$ " + valorTotal);
+    }
+}
