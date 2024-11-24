989d15a02a0706c8ff1854ef8a37be1e754fef25
codelize <lflizier@gmail.com>
2024-11-24 10:16:57 -0300

    Implementação de programas basicos de lógica em Java.

---
+++ src/Fatorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número para calcular o fatorial: ");
+        int numero = scanner.nextInt();
+        long fatorial = 1; // Use 'long' para números grandes
+
+        for (int i = 1; i <= numero; i++) {
+            fatorial *= i;
+        }
+
+        System.out.println("O fatorial de " + numero + " é: " + fatorial);
+
+        scanner.close();
+    }
+}
